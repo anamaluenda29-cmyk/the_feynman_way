a64e89ab1098ad6a18cfd81a195197158c9c0784
anamaluenda29-cmyk <anamaluenda29@gmail.com>
2026-09-16 01:03:08 -0300

    Update app.py
    codigo del programa y su funcionamiento y las directices para el asistente de IA

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if duda_usuario:
 
     # 6. LLAMADO AL MOTOR ESTABLE DE INTELIGENCIA ARTIFICIAL
     response = client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-2.0-flash',
         contents=prompt_final
     )
     
